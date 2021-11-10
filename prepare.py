8c5dd640ef3ebb6b7946fcc35b55164e4ad4050c
Brian Clements <brian.l.clements03@gmail.com>
2021-11-10 12:30:38 -0600

    udpate files, add prepare.py

--- prepare.py
+++ prepare.py
@@ -2,6 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+# import splitting and imputing functions
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+# turn off pink boxes for demo
+import warnings
+warnings.filterwarnings("ignore")
+
+# import our own acquire module
+import acquire
+
 def clean_data(df):
     '''
     This function will clean the data etc etc...

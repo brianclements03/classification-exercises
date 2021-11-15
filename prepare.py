d8546b5b13a0f78654a4f21ab12029fb604ad3fd
Brian Clements <brian.l.clements03@gmail.com>
2021-11-15 12:04:54 -0600

    update exercises and functions

--- prepare.py
+++ prepare.py
@@ -19,7 +19,7 @@ def clean_data(df):
     '''
     
     df = df.drop_duplicates()
-    cols_to_drop = ['deck', 'embarked', 'class', 'age']
+    cols_to_drop = ['deck', 'embarked', 'class']
     df = df.drop(columns = cols_to_drop)
     df['embark_town'] = df.embark_town.fillna(value='Southampton')
     dummy_df = pd.get_dummies(df[['sex', 'embark_town']], dummy_na=False, drop_first=[True,True])

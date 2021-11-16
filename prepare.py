85e206a684d62f86beeee0f282141360bed67b86
Brian Clements <brian.l.clements03@gmail.com>
2021-11-16 12:12:40 -0600

    update and create exercises

--- prepare.py
+++ prepare.py
@@ -53,3 +53,28 @@ def prep_titanic_data(df):
     df = clean_data(df)
     train, validate, test = split_data(df)
     return train, validate, test
+
+
+def prep_telco(df):
+    df = df.drop_duplicates()
+    cols_to_drop = ['payment_type_id', 'internet_service_type_id', 'contract_type_id']
+    df = df.drop(columns = cols_to_drop)
+    df.total_charges = df.total_charges.replace(' ',0)
+    df.total_charges = df.total_charges.astype(float)
+    cols_to_dummy = df[['gender','partner','dependents','phone_service','multiple_lines',
+                        'online_security','online_backup','device_protection','tech_support','streaming_tv',
+                        'streaming_movies','paperless_billing','churn','contract_type',
+                        'internet_service_type','payment_type']]
+    dummy_df = pd.get_dummies(cols_to_dummy, dummy_na=False, drop_first=True)
+    df = pd.concat([df, dummy_df], axis = 1)
+
+    return df
+
+def split_telco_data(df):
+    '''
+    Takes in a dataframe and returns train, validate, test sbusert dataframes
+    '''
+    telco_train, telco_test = train_test_split(df, test_size = .2, stratify=df.churn_Yes)
+    telco_train, telco_validate = train_test_split(telco_train, test_size=.3, stratify=telco_train.churn_Yes)
+    return telco_train, telco_validate, telco_test
+
